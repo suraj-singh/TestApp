6fbd3d5400b898f3499c86f4ac720ab91f7167cf
surajsingh <surajmehta.125@gmail.com>
2014-06-08 21:26:46 +0530

    signout layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,10 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
